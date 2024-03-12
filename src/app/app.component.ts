d12c0f6791d4bff54adc8b49c99ee69b15128f5b
Radoslaw Stefanski <cabreraytpl@gmail.com>
2024-03-12 11:34:41 +0100

    add validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,33 +21,48 @@ export class AppComponent {
   test(): void {
     if (this.game == 'CS2')
       if (this.playersCS.length < 5)
-        this.playersCS.push(
-          new Player(
-            this.playersCS.length + 1,
-            this.name,
-            this.surname,
-            this.nick
-          )
-        );
+        if (
+          this.name.length > 0 &&
+          this.surname.length > 0 &&
+          this.nick.length > 0
+        )
+          this.playersCS.push(
+            new Player(
+              this.playersCS.length + 1,
+              this.name,
+              this.surname,
+              this.nick
+            )
+          );
     if (this.game == 'Archero')
       if (this.playersArchero.length < 5)
-        this.playersArchero.push(
-          new Player(
-            this.playersArchero.length + 1,
-            this.name,
-            this.surname,
-            this.nick
-          )
-        );
+        if (
+          this.name.length > 0 &&
+          this.surname.length > 0 &&
+          this.nick.length > 0
+        )
+          this.playersArchero.push(
+            new Player(
+              this.playersArchero.length + 1,
+              this.name,
+              this.surname,
+              this.nick
+            )
+          );
     if (this.game == 'Ligi Legend')
       if (this.playersLOL.length < 5)
-        this.playersLOL.push(
-          new Player(
-            this.playersLOL.length + 1,
-            this.name,
-            this.surname,
-            this.nick
-          )
-        );
+        if (
+          this.name.length > 0 &&
+          this.surname.length > 0 &&
+          this.nick.length > 0
+        )
+          this.playersLOL.push(
+            new Player(
+              this.playersLOL.length + 1,
+              this.name,
+              this.surname,
+              this.nick
+            )
+          );
   }
 }
